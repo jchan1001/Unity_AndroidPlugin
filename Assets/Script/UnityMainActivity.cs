401d918c02f40722ad009c5b569561984444b9b7
Chung Chanhoo <jchan1001@naver.com>
2018-09-30 01:34:00 +0900

    Add UniRx, Android Project, aar

---
+++ Assets/Script/UnityMainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class UnityMainActivity : MonoBehaviour {
+    const string PACKAGE_NAME_CONTROLLER = "jchan1001.co.jp.unityplugin.Controller";
+    #if !UNITY_EDITOR && UNITY_ANDROID
+        private static AndroidJavaObject activityContext = null;
+    #endif
+
+    [SerializeField]
+    Button mMoveNextActivityButton;
+
+	// Use this for initialization
+	void Start () {
+#if !UNITY_EDITOR && UNITY_ANDROID
+        using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+            activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+        }
+#endif
+
+        mMoveNextActivityButton.OnClickAsObservable().Subscribe(_ => {
+#if !UNITY_EDITOR && UNITY_ANDROID
+            GetAndroidClass(PACKAGE_NAME_CONTROLLER);
+#endif
+        });
+    }
+
+    void MoveActivity(string activityName) {
+
+    }
+
+    AndroidJavaClass GetAndroidClass(string package) {
+        using (AndroidJavaClass ajc = new AndroidJavaClass(package))
+        {
+            if (ajc != null)
+            {
+                return ajc;
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/aar_main/MainActivity.java
@@ -0,0 +1,13 @@
+package jchan1001.co.jp.unityplugin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
