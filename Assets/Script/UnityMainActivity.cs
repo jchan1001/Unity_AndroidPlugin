09925fb5862b49e4a6db0470816e5edbcd842416
Chung Chanhoo <jchan1001@naver.com>
2018-10-04 00:58:54 +0900

    Fix error

--- Assets/Script/UnityMainActivity.cs
+++ Assets/Script/UnityMainActivity.cs
@@ -2,28 +2,33 @@
 using UnityEngine.UI;
 using UniRx;
 
+// AndroidJavaObject -> Java class
+
 public class UnityMainActivity : MonoBehaviour {
     [SerializeField]
     Button mMoveNextActivityButton;
 
     const string CLASS_CONTROLLER = "jchan1001.co.jp.unityplugin.Controller.UnityController";
-    private static AndroidJavaObject activityContext = null;
+    private static AndroidJavaObject go = null;
 
     void Start() {
 #if !UNITY_EDITOR && UNITY_ANDROID
+        SetUnityControllerObject(CLASS_CONTROLLER);
         mMoveNextActivityButton.OnClickAsObservable().Subscribe(_ => {
-            AndroidLog("d", "Unity Log", "Log from unity");
+            go.Call("log", "Unity Log", "Log from unity");
         });
 #endif
     }
 
-    void AndroidLog(string type, string tag, string msg) {
+    // Get UnityController Android Object 
+    // Todo return -> error 
+    void SetUnityControllerObject(string packageName) {
 #if !UNITY_EDITOR && UNITY_ANDROID
-        using (AndroidJavaClass ajc = new AndroidJavaClass(CLASS_CONTROLLER))
+        using (AndroidJavaClass ajc = new AndroidJavaClass(packageName))
         {
             if (ajc != null)
             {
-                ajc.CallStatic("Log", type, tag, msg);
+                go = ajc.CallStatic<AndroidJavaObject>("getUnityController");
             }
         }
 #endif
