e400e69ddf92b83fd6ef3a130ddfea1433042098
Chung Chanhoo <jchan1001@naver.com>
2018-10-05 00:11:13 +0900

    Add move activity sample

--- Assets/Script/UnityMainActivity.cs
+++ Assets/Script/UnityMainActivity.cs
@@ -4,25 +4,31 @@ using UniRx;
 
 // AndroidJavaObject -> Java class
 
-public class UnityMainActivity : MonoBehaviour {
+public class UnityMainActivity : MonoBehaviour
+{
     [SerializeField]
     Button mMoveNextActivityButton;
 
+    private static AndroidJavaObject mActivityContext = null;
     const string CLASS_CONTROLLER = "jchan1001.co.jp.unityplugin.Controller.UnityController";
     private static AndroidJavaObject go = null;
 
-    void Start() {
+    void Start()
+    {
 #if !UNITY_EDITOR && UNITY_ANDROID
         SetUnityControllerObject(CLASS_CONTROLLER);
+        SetContext();
         mMoveNextActivityButton.OnClickAsObservable().Subscribe(_ => {
             go.Call("log", "Unity Log", "Log from unity");
+            go.CallStatic("moveActivity", mActivityContext);
         });
 #endif
     }
 
     // Get UnityController Android Object 
     // Todo return -> error 
-    void SetUnityControllerObject(string packageName) {
+    void SetUnityControllerObject(string packageName)
+    {
 #if !UNITY_EDITOR && UNITY_ANDROID
         using (AndroidJavaClass ajc = new AndroidJavaClass(packageName))
         {
@@ -32,5 +38,14 @@ public class UnityMainActivity : MonoBehaviour {
             }
         }
 #endif
+    }
+
+    void SetContext()
+    {
+        using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            mActivityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+        }
+
     }
 }
