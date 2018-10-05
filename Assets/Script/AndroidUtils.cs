93ef8ce70af1871e897df9dd7fe45438cb0265fe
Chung Chanhoo <jchan1001@naver.com>
2018-10-06 00:49:22 +0900

    Add sample

---
+++ Assets/Script/AndroidUtils.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidUtils {
+    public const string LOG_TYPE_DEBUG = "d";
+    public const string LOG_TYPE_ERROR = "e";
+    public const string LOG_TYPE_WARNING = "w";
+    public const string LOG_TYPE_INFO = "i";
+}
--- Assets/Script/UnityMainActivity.cs
+++ Assets/Script/UnityMainActivity.cs
@@ -11,41 +11,48 @@ public class UnityMainActivity : MonoBehaviour
 
     private static AndroidJavaObject mActivityContext = null;
     const string CLASS_CONTROLLER = "jchan1001.co.jp.unityplugin.Controller.UnityController";
-    private static AndroidJavaObject go = null;
+    private static AndroidJavaObject mUnityController = null;
+    CompositeDisposable mDisposable = new CompositeDisposable();
 
     void Start()
     {
-#if !UNITY_EDITOR && UNITY_ANDROID
-        SetUnityControllerObject(CLASS_CONTROLLER);
-        SetContext();
-        mMoveNextActivityButton.OnClickAsObservable().Subscribe(_ => {
-            go.Call("log", "Unity Log", "Log from unity");
-            go.CallStatic("moveActivity", mActivityContext);
-        });
-#endif
-    }
-
-    // Get UnityController Android Object 
-    // Todo return -> error 
-    void SetUnityControllerObject(string packageName)
-    {
-#if !UNITY_EDITOR && UNITY_ANDROID
-        using (AndroidJavaClass ajc = new AndroidJavaClass(packageName))
+//#if !UNITY_EDITOR && UNITY_ANDROID
+        // Set Unity Controller
+        using (AndroidJavaClass ajc = new AndroidJavaClass(CLASS_CONTROLLER))
         {
             if (ajc != null)
             {
-                go = ajc.CallStatic<AndroidJavaObject>("getUnityController");
+                mUnityController = ajc.CallStatic<AndroidJavaObject>("getUnityController");
             }
         }
-#endif
-    }
 
-    void SetContext()
-    {
+        // Set Context
         using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
         {
             mActivityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
         }
+        
+        // Subscribe
+        mMoveNextActivityButton.OnClickAsObservable().Subscribe(_ => {
+            // Static method call
+            mUnityController.CallStatic("log", AndroidUtils.LOG_TYPE_DEBUG, "Log from unity");
 
+            mUnityController.Call("setTestInt", 10);
+            mUnityController.Call("setTestString", "stringTest");
+            string[] testArr = new string[3];
+            testArr[0] = "0";
+            testArr[1] = "1";
+            testArr[2] = "2";
+            // Todo Edd send error
+            mUnityController.Call("setTestStringArr", (object) testArr);
+
+            mUnityController.Call("moveActivity", mActivityContext);
+        }).AddTo(mDisposable);
+//#endif
+    }
+
+    private void OnDestroy()
+    {
+        mDisposable.Dispose();
     }
 }
